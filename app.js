9608256f20a70d1fc368295228c5d8f374137aa1
Wesley Fonseca <wfonseca18@gmail.com>
2018-10-25 20:14:42 -0400

    Adding responsive navbar

---
+++ app.js
@@ -0,0 +1,9 @@
+
+if (screen.width >= 1024) {
+    $('.responsive-header').removeClass('col-sm-12');
+    $('.responsive-header').addClass('col-sm-6');
+}
+
+$('#icon').on('click', function(){
+    $('.px-3').toggleClass('responsive');
+})
